39bacd5cecf217d50aae34135e1efe49769f3b0c
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-12-08 11:27:19 +0300

    feat: implement deprefixwithoutid

--- prefix/prefix.go
+++ prefix/prefix.go
@@ -1,13 +1,25 @@
 package prefix
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 // Takes a string and an id of a record and adds the id as a prefix to the string
-func PrefixWithId(s string, id int) string{
-return strconv.Itoa(id) + "-" + s
+func PrefixWithId(s string, id int) string {
+	return strconv.Itoa(id) + "-" + s
 }
 
 // Takes a prefixed string and an id of a record and removes the id as a prefix to the string
-func DePrefixWithId(s string, id int) string{
-return s[len(strconv.Itoa(id)) + 1: ]
+func DePrefixWithId(s string, id int) string {
+	return s[len(strconv.Itoa(id))+1:]
+}
+
+/*
+Takes a prefixed string and removes a presumed id as a prefix to the string.
+
+Warning: user has to be sure the string was prefixed otherwise te function would yield inaccurate results
+*/
+func DePrefixWithoutId(s string) string {
+	return s[strings.Index(s, "-")+1:]
 }
